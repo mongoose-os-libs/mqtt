4cca75c84d6183b0cbb48dac659e7c44b5059cdf
Andrea Cioni <supercionci@hotmail.it>
2019-06-18 00:25:51 +0200

    Update function description

--- include/mgos_mqtt.h
+++ include/mgos_mqtt.h
@@ -82,10 +82,10 @@ bool mgos_mqtt_global_is_connected(void);
 
 /*
  * Publish message to the configured MQTT server, to the given MQTT topic.
- * Return value will be true if there is a connection to the server and the
- * message has been queued for sending. In case of QoS 1 return value does
- * not indicate that PUBACK has been received; there is currently no way to
- * check for that.
+ * Return value will be the packet id (> 0) if there is a connection to the server and the
+ * message has been queued for sending. In case no connection is available, 0 is returned.
+ * In case of QoS 1 return value does not indicate that PUBACK has been received; 
+ * there is currently no way to check for that.
  */
 uint16_t mgos_mqtt_pub(const char *topic, const void *message, size_t len, int qos,
                    bool retain);
