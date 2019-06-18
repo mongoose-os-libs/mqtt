c180e2b79177ab6107d06263f3c4604dffd6a1ca
Andrea Cioni <supercionci@hotmail.it>
2019-06-18 21:53:55 +0200

    Return packet id when successfully publish a message (#11)
    With a packet_id the user is allowed to check if a specific message was correctly sent to a broker (QoS > 0). This could be done in a custom event handler where, now, we can compare the returned packet_id with the one contained in ev_data.
    
    CL: Make mgos_mqtt_pub() return packet id when successfully publish a message

--- include/mgos_mqtt.h
+++ include/mgos_mqtt.h
@@ -82,19 +82,19 @@ bool mgos_mqtt_global_is_connected(void);
 
 /*
  * Publish message to the configured MQTT server, to the given MQTT topic.
- * Return value will be true if there is a connection to the server and the
- * message has been queued for sending. In case of QoS 1 return value does
- * not indicate that PUBACK has been received; there is currently no way to
- * check for that.
+ * Return value will be the packet id (> 0) if there is a connection to the
+ * server and the message has been queued for sending. In case no connection is
+ * available, 0 is returned. In case of QoS 1 return value does not indicate
+ * that PUBACK has been received; there is currently no way to check for that.
  */
-bool mgos_mqtt_pub(const char *topic, const void *message, size_t len, int qos,
-                   bool retain);
+uint16_t mgos_mqtt_pub(const char *topic, const void *message, size_t len,
+                       int qos, bool retain);
 
 /* Variant of mgos_mqtt_pub for publishing a JSON-formatted string */
-bool mgos_mqtt_pubf(const char *topic, int qos, bool retain,
-                    const char *json_fmt, ...);
-bool mgos_mqtt_pubv(const char *topic, int qos, bool retain,
-                    const char *json_fmt, va_list ap);
+uint16_t mgos_mqtt_pubf(const char *topic, int qos, bool retain,
+                        const char *json_fmt, ...);
+uint16_t mgos_mqtt_pubv(const char *topic, int qos, bool retain,
+                        const char *json_fmt, va_list ap);
 
 /*
  * Callback signature for `mgos_mqtt_sub()` below.
