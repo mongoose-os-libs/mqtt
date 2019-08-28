615da0fc264e713ee90c823af3a1bcff12739e89
Deomid "rojer" Ryabkov <rojer@rojer.me>
2019-08-28 10:12:56 +0200

    Apply clang-format

--- src/mgos_mqtt.c
+++ src/mgos_mqtt.c
@@ -150,9 +150,9 @@ static void mgos_mqtt_ev(struct mg_connection *nc, int ev, void *ev_data,
       opts.keep_alive = s_cfg->keep_alive;
       opts.will_topic = s_cfg->will_topic;
       opts.will_message = s_cfg->will_message;
-	  if (s_cfg->will_retain) {
-		opts.flags |= MG_MQTT_WILL_RETAIN;
-	  }
+      if (s_cfg->will_retain) {
+        opts.flags |= MG_MQTT_WILL_RETAIN;
+      }
       const char *client_id =
           (s_cfg->client_id != NULL ? s_cfg->client_id
                                     : mgos_sys_config_get_device_id());
